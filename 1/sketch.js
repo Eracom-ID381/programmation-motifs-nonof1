12539c2ab6a4641a65ddd91de0614026ee3e70cf
nonof1 <60931115+nonof1@users.noreply.github.com>
2020-04-08 14:05:20 +0200

    setting up first grid

--- 1/sketch.js
+++ 1/sketch.js
@@ -1,21 +1,25 @@
 function setup() {
-  let navWidth = document.getElementById("nav").offsetWidth;
-  let canvas = createCanvas(windowWidth - navWidth, windowHeight);
-  canvas.parent('canvas');
-  background(255);
+    let navWidth = document.getElementById("nav").offsetWidth;
+    let canvas = createCanvas(windowWidth - navWidth, windowHeight);
+    canvas.parent('canvas');
+    background(255);
 }
 
 function draw() {
-  let x = 10;
-  fill(0);
-  while (x < width) {
-    ellipse(x, height / 2, 10, 10);
-    x = x + 30;
-  }
+    stroke(0);
+    strokeWeight(6);
+
+    for (let x = 0; x < windowWidth; x = x + 100;) {
+        for (let y = 0; y < windowHeight; y = y + 100) {
+
+        }
+
+    }
+
 }
 
 function windowResized() {
-  let navWidth = document.getElementById("nav").offsetWidth;
-  resizeCanvas(windowWidth - navWidth, windowHeight);
-  background(255);
+    let navWidth = document.getElementById("nav").offsetWidth;
+    resizeCanvas(windowWidth - navWidth, windowHeight);
+    background(255);
 }
