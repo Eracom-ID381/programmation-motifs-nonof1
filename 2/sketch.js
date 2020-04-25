8ec1c261659768d6160adf3dd6175930d6fcd5d4
nonof1 <60931115+nonof1@users.noreply.github.com>
2020-04-25 08:53:05 +0200

    Motifs 2&3 added

--- 2/sketch.js
+++ 2/sketch.js
@@ -3,16 +3,19 @@ function setup() {
     let canvas = createCanvas(windowWidth - navWidth, windowHeight);
     canvas.parent('canvas');
     background(255);
+    stroke(0);
+    strokeWeight(2);
+    for (let x = 0; x < width; x = x + 30) {
+        for (let y = 0; y < height; y = y + 30) {
+            rect(x, y, random(0, 30), random(0, 30));
+            rect(x, y, random(0, 30), random(0, 30));
+            rect(x, y, random(0, 30), random(0, 30));
+        }
+    }
 }
 
 function draw() {
-    stroke(0);
-    strokeWeight(6);
-    for (let x = 0; x < width; x = x + 150) {
-        for (let y = 0; y < height; y = y + 150) {
 
-        }
-    }
 }
 
 function windowResized() {
--- 3/sketch.js
+++ 3/sketch.js
@@ -1,21 +1,23 @@
 function setup() {
-  let navWidth = document.getElementById("nav").offsetWidth;
-  let canvas = createCanvas(windowWidth - navWidth, windowHeight);
-  canvas.parent('canvas');
-  background(255);
+    let navWidth = document.getElementById("nav").offsetWidth;
+    let canvas = createCanvas(windowWidth - navWidth, windowHeight);
+    canvas.parent('canvas');
+    background(255);
+    colorMode(HSB, 100);
+    stroke(random(0, 100), random(50, 100), random(50, 100));
+    for (let x = 0; x < width; x = x + random(0, 30)) {
+        for (let y = 0; y < height; y = y + random(0, 20)) {
+            ellipse(x, y, random(0, 20), random(0, 20));
+        }
+    }
 }
 
 function draw() {
-  let x = 10;
-  fill(0);
-  while (x < width) {
-    ellipse(x, height / 2, 10, 10);
-    x = x + 30;
-  }
+
 }
 
 function windowResized() {
-  let navWidth = document.getElementById("nav").offsetWidth;
-  resizeCanvas(windowWidth - navWidth, windowHeight);
-  background(255);
+    let navWidth = document.getElementById("nav").offsetWidth;
+    resizeCanvas(windowWidth - navWidth, windowHeight);
+    background(255);
 }
