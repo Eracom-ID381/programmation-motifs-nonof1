bced4ccd123b62d523142bf1067ae2ea085948e7
nonof1 <60931115+nonof1@users.noreply.github.com>
2020-04-25 09:52:53 +0200

    Unfinished 5th pattern --> need help with rotate animation

--- 5/sketch.js
+++ 5/sketch.js
@@ -3,10 +3,17 @@ function setup() {
     let canvas = createCanvas(windowWidth - navWidth, windowHeight, WEBGL);
     canvas.parent('canvas');
     background(255);
+    noStroke();
+    colorMode(HSB, 100);
+    fill(random(0, 100), random(50, 100), random(50, 100));
 }
 
 function draw() {
-
+    let angle = 0;
+    angle = angle + 0.07;
+    rectMode(CENTER);
+    rotateY(angle);
+    rect(0, 0, 150, 150);
 }
 
 function windowResized() {
