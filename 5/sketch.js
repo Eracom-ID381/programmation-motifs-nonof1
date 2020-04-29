477a12227c09fa3e0dc94d001c0631272fdfc180
Simon Pinkas <simon@code-think.com>
2020-04-29 13:47:21 +0200

    Fixed boxes

--- 5/sketch.js
+++ 5/sketch.js
@@ -1,23 +1,30 @@
 function setup() {
-    let navWidth = document.getElementById("nav").offsetWidth;
-    let canvas = createCanvas(windowWidth - navWidth, windowHeight, WEBGL);
-    canvas.parent('canvas');
-    background(255);
-    noStroke();
-    colorMode(HSB, 100);
-    fill(random(0, 100), random(50, 100), random(50, 100));
+  let navWidth = document.getElementById("nav").offsetWidth;
+  let canvas = createCanvas(windowWidth - navWidth, windowHeight, WEBGL);
+  canvas.parent('canvas');
+  background(255);
+  //  noStroke();
+  colorMode(HSB, 100);
+  // fill(random(0, 100), random(50, 100), random(50, 100));
 }
 
 function draw() {
-    let angle = 0;
-    angle = angle + 0.07;
-    rectMode(CENTER);
-    rotateY(angle);
-    rect(0, 0, 150, 150);
+  rectMode(CENTER);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  for (let x = 0; x < width; x = x + 50) {
+    for (let y = 0; y < height; y = y + 50) {
+      translate(x, y);
+      box(noise(x, y) * 100);
+
+
+    }
+
+  }
 }
 
 function windowResized() {
-    let navWidth = document.getElementById("nav").offsetWidth;
-    resizeCanvas(windowWidth - navWidth, windowHeight);
-    background(255);
+  let navWidth = document.getElementById("nav").offsetWidth;
+  resizeCanvas(windowWidth - navWidth, windowHeight);
+  background(255);
 }
