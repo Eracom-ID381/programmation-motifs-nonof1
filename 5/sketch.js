86b753048fa64081970316f07013a2d8fd50af84
nonof1 <60931115+nonof1@users.noreply.github.com>
2020-04-29 13:59:25 +0200

    added color

--- 5/sketch.js
+++ 5/sketch.js
@@ -1,30 +1,28 @@
 function setup() {
-  let navWidth = document.getElementById("nav").offsetWidth;
-  let canvas = createCanvas(windowWidth - navWidth, windowHeight, WEBGL);
-  canvas.parent('canvas');
-  background(255);
-  //  noStroke();
-  colorMode(HSB, 100);
-  // fill(random(0, 100), random(50, 100), random(50, 100));
+    let navWidth = document.getElementById("nav").offsetWidth;
+    let canvas = createCanvas(windowWidth - navWidth, windowHeight, WEBGL);
+    canvas.parent('canvas');
+    background(255);
+    //  noStroke();
+    colorMode(HSB, 100);
+    fill(random(0, 100), random(50, 100), random(50, 100));
 }
 
 function draw() {
-  rectMode(CENTER);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  for (let x = 0; x < width; x = x + 50) {
-    for (let y = 0; y < height; y = y + 50) {
-      translate(x, y);
-      box(noise(x, y) * 100);
-
+    rectMode(CENTER);
+    rotateX(frameCount * 0.01);
+    rotateY(frameCount * 0.01);
+    for (let x = 0; x < width; x = x + 50) {
+        for (let y = 0; y < height; y = y + 50) {
+            translate(x, y);
+            box(noise(x, y) * 100);
+        }
 
     }
-
-  }
 }
 
 function windowResized() {
-  let navWidth = document.getElementById("nav").offsetWidth;
-  resizeCanvas(windowWidth - navWidth, windowHeight);
-  background(255);
+    let navWidth = document.getElementById("nav").offsetWidth;
+    resizeCanvas(windowWidth - navWidth, windowHeight);
+    background(255);
 }
